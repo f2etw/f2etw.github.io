598f5b9310e7699bf30da971117762f5e4544e68
WM Chang <wandererm@gmail.com>
2014-11-25 12:26:21 +0800

    fix for metalsmith compiling

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		watch: {
 			livereload: {
-				files: ['**/*.md', '**/*.html', '!node_modules/**/*'],
+				files: ['**/*.md', '**/*.html', '!node_modules/**/*', '!build/**/*', '!_site/**/*'],
 				tasks: ['copy', 'metalsmith'],
 				options: {
 					livereload: true
@@ -18,13 +18,23 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+		clean: ['src'],
 		copy: {
 			main: {
 				files: [{
 					expand: true,
-					src: ['**/*.md', '**/*.scss', '!node_modules/**/*', '!_sites/**/*', '!src/**/*'],
+					src: ['**/*.md', '!node_modules/**/*', '!_sites/**/*', '!src/**/*'],
 					dest: 'src/'
 				}]
+			},
+			style: {
+				src: 'style.scss',
+				dest: 'src/style.scss',
+				options: {
+					process: function (content, srcpath) {
+						return content.replace(/---\s.*\s---\s/, '');
+					}
+				}
 			}
 		},
 		metalsmith: {
@@ -37,7 +47,7 @@ module.exports = function(grunt) {
 					},
 					"metalsmith-templates": {
 					  "engine": "swig",
-					  "directory": "src/_layouts",
+					  "directory": "_layouts",
 					  "default": "default.html",
 					  "pattern": ["!*.css"]
 					}
@@ -51,7 +61,9 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-metalsmith');
 
-	grunt.registerTask('default', ['copy', 'metalsmith', 'connect', 'watch']);
+	grunt.registerTask('default', ['clean', 'copy', 'metalsmith', 'connect', 'watch']);
+	grunt.registerTask('build', ['clean', 'copy', 'metalsmith']);
 };
