53f4c4d2b4d9d107060b46750fffd4fd4cab6c71
Rplus <rplus.tw@gmail.com>
2016-02-17 21:13:12 +0800

    調整 youtube plugin 為 RWD style

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -2,6 +2,9 @@
 class YouTube < Liquid::Tag
   Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
 
+  # $1: youtube embed id
+  # $2: video player aspect ratio
+
   def initialize(tagName, markup, tokens)
     super
 
@@ -9,11 +12,9 @@ class YouTube < Liquid::Tag
       @id = $1
 
       if $2.nil? then
-          @width = 560
-          @height = 420
+        @ratio = 100 * 9/16
       else
-          @width = $2.to_i
-          @height = $3.to_i
+        @ratio = $2.to_i
       end
     else
       raise "No YouTube ID provided in the \"youtube\" tag"
@@ -21,8 +22,7 @@ class YouTube < Liquid::Tag
   end
 
   def render(context)
-    # "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\"allowfullscreen></iframe>"
-    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\"></iframe>"
+    "<div class=\"yt-box\"><div class=\"yt-box__placeholder\" style=\"padding-bottom: #{@ratio}%\"></div><iframe class=\"yt-box__iframe\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\" allowfullscreen></iframe></div>"
   end
 
   Liquid::Template.register_tag "youtube", self
