1c21eb12bccb008b20862bc9800c9173c1a75b62
Rplus <rplus.tw@gmail.com>
2016-02-17 20:42:15 +0800

    remove useless youtube url parameter

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -22,7 +22,7 @@ class YouTube < Liquid::Tag
 
   def render(context)
     # "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\"allowfullscreen></iframe>"
-    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}?color=white&theme=light\"></iframe>"
+    "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\"></iframe>"
   end
 
   Liquid::Template.register_tag "youtube", self
